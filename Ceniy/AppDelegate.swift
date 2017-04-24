8f42dd7d288dba5ceb7d08b52cd37768faeb86f7
wwgkzy <wwgkzy@163.com>
2017-04-24 17:15:46 +0800

    1.0
    Begin

---
+++ Ceniy/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Ceniy
+//
+//  Created by minghao on 2017/4/24.
+//  Copyright © 2017年 Ceniy. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+        return true
+    }
+}
+
